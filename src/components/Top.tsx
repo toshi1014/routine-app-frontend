7f60b222e4675e38167bf96b7cfa30f4695ad39e
toshi1014 <toshihide1014go@gmail.com>
2021-11-28 00:59:24 +0900

    add: base comp

---
+++ src/components/Top.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Button,
+} from "@mui/material";
+
+
+function Top() {
+    return (
+        <div>
+            <h1>Top</h1>
+        </div>
+    );
+}
+
+export default Top;
