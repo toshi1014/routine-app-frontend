f9cdfcccf184ab827412ac3e0915f8d66088d8b6
toshi1014 <toshihide1014go@gmail.com>
2021-11-29 01:08:55 +0900

    add: ImageSlider at Top page

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import {
-    Button,
+    Paper,
+    Card,
+    Grid,
+    CardContent,
+    Stack,
+    Container,
+    Chip,
 } from "@mui/material";
+import ImageSlider from './ImageSlider';
+
+// TEMP:
+const hashtagList = [
+    "fishing",
+    "hoby",
+];
 
 
 function Top() {
+    const polularHashtagChipList = hashtagList.map((hashtag: string) =>
+        <Chip clickable label={"# " + hashtag} />
+    );
+
+    const polularRoutineList = hashtagList.map((hashtag: string) =>
+        <Chip clickable label={"# " + hashtag} />
+    );
+
     return (
         <div>
-            <h1>Top</h1>
+            <ImageSlider />
+            <Grid container direction="column" spacing={3}>
+                <Grid item>
+                    <Paper>
+                        <Container>
+                            <h1>Expand Your Routine</h1>
+                            <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
+                        </Container>
+                    </Paper>
+                </Grid>
+
+                <Grid item>
+                    <CardContent>
+                        <h2>Polular Hashtags</h2>
+                        <Stack direction="row" spacing={1}>
+                            {polularHashtagChipList}
+                        </Stack>
+                    </CardContent>
+                </Grid>
+
+                <Grid item>
+                    <CardContent>
+                        <h2>Polular Routines</h2>
+                        <Stack direction="row" spacing={1}>
+                            {polularRoutineList}
+                        </Stack>
+                    </CardContent>
+                </Grid>
+            </Grid>
         </div>
     );
 }
