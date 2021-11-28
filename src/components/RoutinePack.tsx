7736276770f4a21ed64ecb9e7fe372beca064011
toshi1014 <toshihide1014go@gmail.com>
2021-11-29 02:40:46 +0900

    add: RoutinePack at Top page

---
+++ src/components/RoutinePack.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardMedia,
+    CardContent,
+    CardActions,
+    Typography,
+    Avatar,
+    Collapse,
+} from "@mui/material";
+import {
+    styled,
+} from "@mui/material/styles";
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ShareIcon from "@mui/icons-material/Share";
+import TextWithLimitation from "./TextWithLimitation";
+
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+type Props = {
+    contributor: string;
+    title: string;
+    desc: string;
+    lastUpdated: string;
+    titleStep1: string;
+    descStep1: string;
+}
+
+function RoutinePack(props: Props) {
+    const avatarSize = 35;
+    const packMaxWidth = 345;
+
+    const [expanded, setExpanded] = React.useState(false);
+    const handleExpandMoreClick = () => {
+        setExpanded(!expanded);
+    };
+
+
+    return (
+        <div>
+            <Card sx={{ maxWidth: packMaxWidth }}>
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            alt="Smiley"
+                            src="static/demo/face.png"
+                            sx={{ width: avatarSize, height: avatarSize }}
+                        />
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={props.title}
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image="logo192.png"
+                    alt="Paella dish"
+                />
+
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        <TextWithLimitation
+                            maxWidth={packMaxWidth}
+                            text={props.desc}
+                        />
+                    </Typography>
+                </CardContent>
+
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandMoreClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <TextWithLimitation
+                            maxWidth={packMaxWidth}
+                            text={"1. " + props.titleStep1}
+                        />
+                        <TextWithLimitation
+                            maxWidth={packMaxWidth}
+                            text={props.descStep1}
+                        />
+                        <Typography paragraph>...</Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
+    );
+}
+
+export default RoutinePack;
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
     Paper,
-    Card,
     Grid,
     CardContent,
     Stack,
@@ -9,12 +8,20 @@ import {
     Chip,
 } from "@mui/material";
 import ImageSlider from './ImageSlider';
+import RoutinePack from './RoutinePack';
+import { range } from "../utils/utils";
 
 // TEMP:
 const hashtagList = [
     "fishing",
     "hoby",
 ];
+const contributor = "John Smith";
+const title = "Happy Coding";
+const desc = "Best Way to Create App, set aside off of the heat to let rest for 10 minutes, and then serve.";
+const lastUpdated = "2021, Dec 31";
+const titleStep1 = "Buy Computer";
+const descStep1 = "Choose best computer for you, set aside off of the heat to let rest for 10 minutes, and then serve.";
 
 
 function Top() {
@@ -22,13 +29,21 @@ function Top() {
         <Chip clickable label={"# " + hashtag} />
     );
 
-    const polularRoutineList = hashtagList.map((hashtag: string) =>
-        <Chip clickable label={"# " + hashtag} />
+    const polularRoutineList = range(0, 5).map((idx: number) =>
+        <RoutinePack
+            contributor={contributor}
+            title={title}
+            desc={desc}
+            lastUpdated={lastUpdated}
+            titleStep1={titleStep1}
+            descStep1={descStep1}
+        />
     );
 
     return (
         <div>
             <ImageSlider />
+
             <Grid container direction="column" spacing={3}>
                 <Grid item>
                     <Paper>
