ebe3bfa17b154db3f2fb4711f62f1249eb2d167f
toshi1014 <toshihide1014go@gmail.com>
2021-11-30 22:30:57 +0900

    improve: RoutineContents layout

--- src/components/RoutineContents.tsx
+++ src/components/RoutineContents.tsx
@@ -10,7 +10,6 @@ import {
     Card,
     Typography,
     CardHeader,
-    CardMedia,
     Button,
     Box,
     IconButton,
@@ -87,30 +86,49 @@ function RoutineContents() {
                                 <Stack direction="row" spacing={1}>
                                     {hashtagChipList}
                                 </Stack>
-
                             </Grid>
+
                             <Grid item>
-                                <Stack direction="row" spacing={1}>
-                                    <FavoriteIcon />
-                                    <h4>30</h4>
+                                <Stack
+                                    direction="row"
+                                    spacing={1}
+                                    sx={{
+                                        my: 1,
+                                    }}
+                                >
+                                    <FavoriteIcon
+                                        sx={{ my: -0.4 }}
+                                    />
+                                    <Typography>
+                                        30
+                                    </Typography>
+
                                 </Stack>
                             </Grid>
                         </Grid>
                     </Grid>
 
-                    <Grid item xs={2}>
+                    <Grid item xs={2} sx={{my:2}}>
                         <Paper variant="outlined">
                             <Container>
-                                <Grid container spacing={2} direction="column">
+                                <Grid container spacing={0} direction="column">
                                     <Grid item>
-                                        <Stack direction="row">
-                                            <Avatar
-                                                alt="Smiley"
-                                                src="static/demo/face.png"
-                                                sx={{ width: avatarSize, height: avatarSize }}
-                                            />
-                                            <h3>{contributor}</h3>
-                                        </Stack>
+                                        <Grid container direction="row" spacing={1}>
+                                            <Grid item>
+                                                <Avatar
+                                                    alt="Smiley"
+                                                    src="static/demo/face.png"
+                                                    sx={{
+                                                        width: avatarSize,
+                                                        height: avatarSize,
+                                                        my: 1.2,
+                                                    }}
+                                                />
+                                            </Grid>
+                                            <Grid item>
+                                                <h3>{contributor}</h3>
+                                            </Grid>
+                                        </Grid>
                                         <Chip clickable variant="outlined" label="follow" />
                                     </Grid>
 
@@ -119,7 +137,12 @@ function RoutineContents() {
                                     </Grid>
 
                                     <Grid item>
-                                        <Stack direction="row">
+                                        <Stack
+                                            direction="row"
+                                            sx={{
+                                                my: 1,
+                                            }}
+                                        >
                                             <IconButton onClick={handleFavorite}>
                                                 <FavoriteIcon />
                                             </IconButton>
@@ -140,38 +163,40 @@ function RoutineContents() {
 
     return (
         <div>
-            <Stack spacing={3}>
+            <Stack spacing={3} direction="column">
                 {header}
 
-                <Stack direction="column" spacing={1}>
+                <Stack direction="column" spacing={0}>
                     {routineElementList.map((routineElement: RoutineElement, idx: number) =>
                         <Card variant="outlined">
-                            <Grid container spacing={2} columns={10}>
-                                <Grid item xs={8}>
-                                    <Card>
-                                        <CardHeader
-                                            title={(idx + 1) + ". " + routineElement.title}
-                                            subheader={routineElement.subtitle}
-                                        />
-                                        <CardContent>
-                                            <Typography paragraph>
-                                                {routineElement.desc}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button size="small">Learn More</Button>
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
+                            <CardContent>
+                                <Grid container spacing={2} columns={10}>
+                                    <Grid item xs={8}>
+                                        <Card>
+                                            <CardHeader
+                                                title={(idx + 1) + ". " + routineElement.title}
+                                                subheader={routineElement.subtitle}
+                                            />
+                                            <CardContent>
+                                                <Typography paragraph>
+                                                    {routineElement.desc}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button size="small">Learn More</Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Grid>
 
-                                <Grid item xs={1}>
-                                    <img
-                                        height="200"
-                                        src={routineElement.imagePath}
-                                        alt="img"
-                                    />
+                                    <Grid item xs={1}>
+                                        <img
+                                            height="200"
+                                            src={routineElement.imagePath}
+                                            alt="img"
+                                        />
+                                    </Grid>
                                 </Grid>
-                            </Grid>
+                            </CardContent>
                         </Card>
                     )}
                 </Stack>
