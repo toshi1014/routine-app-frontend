bc741d188e3e5456c751729dab5f0d3c3f8da41c
toshi1014 <toshihide1014go@gmail.com>
2021-12-01 21:04:08 +0900

    fix: add & remove element in Post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,9 +10,16 @@ import {
     CardContent,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { RoutineHeaderInput, RoutineElementInput } from "../utils/Types";
-import { RoutineHeader, RoutineElement } from "../utils/Types";
+import RemoveIcon from "@mui/icons-material/Remove";
+import {
+    RoutineElement,
+    RoutineHeaderInput,
+    RoutineElementInput,
+    RoutineHeaderRef,
+    RoutineElementRef,
+} from "../utils/Types";
 import ContentsBase from "./ContentsBase";
+
 
 // TEMP:
 const username = "John Smith";
@@ -52,7 +59,7 @@ function Post() {
         lastUpdated: lastUpdated,
     };
 
-    const routineHeaderRef = {
+    const routineHeaderRef: RoutineHeaderRef = {
         title: {
             value: ""
         },
@@ -61,7 +68,7 @@ function Post() {
         },
     }
 
-    const routineElementRef = {
+    const routineElementRef: RoutineElementRef = {
         title: {
             value: ""
         },
@@ -72,42 +79,47 @@ function Post() {
             value: ""
         },
     };
-    const routineElementRefList = [routineElementRef];
+    const [routineElementRefList, setRoutineElementRefList] =
+        React.useState<Array<RoutineElementRef>>([routineElementRef]);
 
-    const routineElement: RoutineElementInput = {
-        title: (
-            <TextField
-                fullWidth
-                label="title"
-                variant="filled"
-                inputRef={ref => { routineElementRefList[0].title = ref; }}
-            />
-        ),
 
-        subtitle: (
-            <TextField
-                fullWidth
-                label="subtitle"
-                variant="standard"
-                sx={{ my: 3 }}
-                inputRef={ref => { routineElementRefList[0].subtitle = ref; }}
-            />
-        ),
+    const getIndexedRoutineElement = (idx: number) => {
+        const routineElement: RoutineElementInput = {
+            title: (
+                <TextField
+                    fullWidth
+                    label="title"
+                    variant="filled"
+                    inputRef={ref => { routineElementRefList[idx].title = ref; }}
+                />
+            ),
 
-        desc: (
-            <TextField
-                fullWidth
-                multiline
-                label="desc"
-                variant="outlined"
-                inputRef={ref => { routineElementRefList[0].desc = ref; }}
-            />
-        ),
+            subtitle: (
+                <TextField
+                    fullWidth
+                    label="subtitle"
+                    variant="standard"
+                    sx={{ my: 3 }}
+                    inputRef={ref => { routineElementRefList[idx].subtitle = ref; }}
+                />
+            ),
 
-        imagePath: "logo192.png",
-    };
+            desc: (
+                <TextField
+                    fullWidth
+                    multiline
+                    label="desc"
+                    variant="outlined"
+                    inputRef={ref => { routineElementRefList[idx].desc = ref; }}
+                />
+            ),
+
+            imagePath: "logo192.png",
+        };
+        return routineElement;
+    }
     const [routineElementList, setRoutineElementList] =
-        React.useState<Array<RoutineElementInput>>([routineElement]);
+        React.useState<Array<RoutineElementInput>>([getIndexedRoutineElement(0)]);
 
     const handleFavorite = () => {
         console.log("Favorite");
@@ -125,29 +137,120 @@ function Post() {
     //     console.log(routineElementList[0].title);
     // }, [routineElementList])
 
-    const handlePost = () => {
-        console.log("title", routineHeaderRef.title.value);
-        console.log("desc", routineHeaderRef.desc.value);
-        routineElementRefList.map((val: any) => {
-            console.log("title", val.title.value);
-            console.log("subtitle", val.subtitle.value);
-            console.log("desc", val.desc.value);
-        })
+    const removeNullFromInput = (routineElementRefList: Array<RoutineElementRef>) => {
+        let withoutNull: Array<RoutineElement> = [];
+
+        // remove null input
+        for (let r of routineElementRefList) {
+            if (r.title === null) {
+                continue;
+            } else {
+                withoutNull.push({
+                    title: r.title.value,
+                    subtitle: r.subtitle.value,
+                    desc: r.desc.value,
+                    imagePath: "logo192.png"
+                })
+            }
+        }
+
+        let withoutNullEmpty: Array<RoutineElement> = [];
+
+        // remove emnty input from end
+        for (let r of withoutNull.reverse()) {
+            if ((r.title + r.subtitle + r.desc) === "") {
+                continue;
+            } else {
+                withoutNullEmpty.push({
+                    title: r.title,
+                    subtitle: r.subtitle,
+                    desc: r.desc,
+                    imagePath: "logo192.png"
+                })
+            }
+        }
+
+        return withoutNullEmpty.reverse();
     };
 
+    const handlePost = () => {
+        const routineElementsInputValue:Array<RoutineElement> = removeNullFromInput(routineElementRefList);
+
+        console.log("============================")
+        console.log("routineHeader")
+        console.log("title:", routineHeaderRef.title.value);
+        console.log("desc:", routineHeaderRef.desc.value);
+        console.log("\nroutineElements")
+
+        for (let i=0; i < routineElementsInputValue.length; i++) {
+            const r = routineElementsInputValue[i];
+            console.log(i+1 + ".");
+            console.log("title:", r.title);
+            console.log("subtitle:", r.subtitle);
+            console.log("desc:", r.desc);
+        }
+    };
+
+
+    // XXX:
+    const [foo, setFoo] = React.useState(0);
+    const update = () => {
+        setFoo(foo + 1);
+    };
+    React.useEffect(() => {
+        if (foo % 2 === 1) {
+            update();
+        }
+    }, [foo]);
+    React.useEffect(() => {
+        update();
+    }, [routineElementList, routineElementRefList]);
+    // end; XXX
+
+
     const addElement = () => {
-        const routineElementListTmp = routineElementList;
-        routineElementListTmp.push(routineElement);
+        let routineElementRefListTmp = routineElementRefList;
+        const routineElementRefListOrgn = routineElementRefListTmp;
+        routineElementRefListTmp.push(routineElementRef);
+        setRoutineElementRefList(routineElementRefListTmp);
+
+        let routineElementListTmp = routineElementList;
+        const newIdx = routineElementListTmp.length;
+        console.log(newIdx);
+        routineElementListTmp.push(getIndexedRoutineElement(newIdx));
         setRoutineElementList(routineElementListTmp);
-        routineElementRefList.push(routineElementRef);
-        console.log(routineElementList.length);
+
+        setDisabledRemoveIcon(false);
+        update();
+    }
+
+    const [disabledRemoveIcon, setDisabledRemoveIcon] = React.useState(true);
+    const removeElement = () => {
+        let routineElementListTmp = routineElementList;
+        routineElementListTmp.pop();
+        setRoutineElementList(routineElementListTmp);
+
+        // XXX: not working
+        // let routineElementRefListTmp = routineElementRefList;
+        // routineElementRefListTmp.pop();
+        // setRoutineElementRefList(routineElementRefListTmp);
+
+        if (routineElementListTmp.length === 1) {
+            setDisabledRemoveIcon(true);
+        };
+        update();
     }
 
 
     const submitButtonsComp = (
         <CardContent>
             <Grid container columns={10}>
-                <Grid item xs={9}>
+                <Grid item xs={8}>
+                </Grid>
+                <Grid item xs={0.8}>
+                    <Fab color="secondary" onClick={removeElement} disabled={disabledRemoveIcon}>
+                        <RemoveIcon />
+                    </Fab>
                 </Grid>
                 <Grid item xs={1}>
                     <Fab color="primary" onClick={addElement}>
